f1e67f101d592cf2a4a904b89928f64925655b29
theashleyromick <ashley.romick@gmail.com>
2024-08-01 15:29:55 -0400

    make code function

--- js/scrips.js
+++ js/scrips.js
@@ -1,40 +1,40 @@
-function newItem() {
-  // Adding a new item to the list of items
+$(document).ready(function () {
+  // Function to add a new item to the list
+  function newItem() {
+    // Adding a new item to the list of items
+    let li = $("<li></li>");
+    let inputValue = $("#input").val();
+    li.append(inputValue);
 
-  let li = $("<li></li>");
-  let inputValue = $("#input").val();
-  li.append(inputValue);
+    if (inputValue === "") {
+      alert("You must write something!");
+    } else {
+      $("#list").append(li);
+      $("#input").val(""); // Clear the input field
+    }
 
-  if (inputValue === "") {
-    alert("You must write something!");
-  } else {
-    $("#list").append(li);
+    // Crossing out an item from the list of items
+    li.on("dblclick", function () {
+      li.toggleClass("strike");
+    });
+
+    // Adding the delete button "X"
+    let crossOutButton = $("<button></button>");
+    crossOutButton.append(document.createTextNode("X"));
+    crossOutButton.addClass("crossOutButton");
+    li.append(crossOutButton);
+
+    crossOutButton.on("click", function () {
+      li.remove();
+    });
+
+    // Reordering the items
+    $("#list").sortable();
   }
 
-  // Crossing out an item from the list of items
-
-  function crossOut() {
-    li.toggleClass("strike");
-  }
-
-  li.on("dblclick", function crossOut() {
-    li.toggleClass("strike");
-  });
-
-  // Adding the delete button "X"
-
-  let crossOutButton = $("<crossOutButton></crossOutButton>");
-  crossOutButton.append(document.createTextNode("X"));
-  li.append(crossOutButton);
-
-  crossOutButton.on("click", deleteListItem);
-  function deleteListItem() {
-    li.addClass("delete");
-  }
-
-  // Reordering the items
-  $("#list").sortable();
-}
+  // Attach event listener to the add button
+  $("#button").on("click", newItem);
+});
 
 // If you get stuck, you can look below for the jQuery code. However, try yourself to convert the vanilla JS code provided to jQuery first.
 
